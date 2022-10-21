533c61d19992d5d674d1219d1158ef64f8ceb037
dermo <dermot.logue@dkit.ie>
2022-10-21 11:32:52 +0100

    start

--- src/main/java/org/example/ClassGroup.java
+++ src/main/java/org/example/ClassGroup.java
@@ -1,2 +1,28 @@
-package org.example;public class ClassGroup {
+package org.example;
+import java.util.ArrayList;
+public class ClassGroup {
+    private String name; // name of the class
+    private ArrayList<String> studentsList; // list of students in the class
+    public ClassGroup(String name) {
+        this.name = name;
+        this.studentsList = new ArrayList<>();  // create the ArrayList object
+    }
+    // access to students is possible only by calling the
+    // methods provided below.
+    public void add(String name) {
+        studentsList.add( name );
+    }
+    public ArrayList<String> getStudentsList() {
+        return this.studentsList;
+        // ArrayList<String> temp = new ArrayList<>( this.studentsList);  // clone
+        // return temp;
+
+        // returns a clone of the arraylist (so not to leak out the reference)
+    }
+
+    public int size() {
+        return studentsList.size();
+    }
+
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,30 @@
-package org.example;public class Main {
+package org.example;
+
+
+import java.util.ArrayList;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        ClassGroup groupA = new ClassGroup("SD2a");
+
+        groupA.add("Kevin");
+        groupA.add("Luana");
+
+        int numStudents = groupA.size();
+        System.out.println("Number of students = " + numStudents);
+
+        ArrayList<String> list = groupA.getStudentsList();
+
+        for( String name : list )
+            System.out.println(name + ", ");
+
+
+
+
+        //ClassGroup groupB = new ClassGroup("SD2b");
+
+
     }
 }
